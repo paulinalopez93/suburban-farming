b6ccb28a55db6ff353708fee9456ce9b615e1651
Paulina Lopez <paulielopez93@gmail.com>
2019-07-23 09:46:10 -0500

    did the controllers with rails g controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
   devise_for :users
   root to: 'pages#home'
-  resources :products, only: [:index, :show]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/profile', to: 'users#profile', as: 'profile'
+  resources :orders, except: [:index]
+  resources :products do
+    resources :reviews, only: [:create, :new]
+  end
 end
