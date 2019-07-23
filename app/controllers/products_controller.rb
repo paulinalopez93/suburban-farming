5559cfb8b5c747866bab3f86168c93f197cbe4fb
(Simon Martinez) <smartineze1993@gmail.com>
2019-07-23 18:00:24 +0200

    Finish the products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,49 @@
 class ProductsController < ApplicationController
+
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
+    @products = Product.all
   end
 
   def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:price, :details, :photo, :category)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
