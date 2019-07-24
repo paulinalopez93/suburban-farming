d08b186a2716e188a9eacd7fe33362f5d0266ee1
Adel Muursepp <adel.muursepp@gmail.com>
2019-07-24 15:16:47 +0300

    working with products browse

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,25 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
     @products = Product.all
+    @categories = Product.categories.keys
+    @order = 0
+    if user_signed_in?
+      if current_user.orders.any?
+        @order = current_user.orders.where(paid: false).last
+      else
+        @order = Order.new(user_id: current_user.id)
+      end
+    else
+      @order = Order.new(user_id: 1)
+    end
+    raise
+    @products.each do |product|
+      product_order = ProductOrder.new
+      product_order.product = product
+      product_order.order = @order
+      product_order.save
+      puts product_order
+    end
   end
 
   def show
