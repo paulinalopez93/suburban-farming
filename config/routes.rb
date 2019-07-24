814194f12f4ea517d0be3fb3a74456a16201bd9a
Paulina Lopez <paulielopez93@gmail.com>
2019-07-24 03:33:38 -0500

    added a payment route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/profile', to: 'users#profile', as: 'profile'
+  get '/payment', to: 'orders#payment', as: 'payment'
   resources :orders, except: [:index]
   resources :products do
     resources :reviews, only: [:create, :new]
