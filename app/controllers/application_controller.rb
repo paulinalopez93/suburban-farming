a9cc7005a887a924c10bdb03e1b3ea0765c7fdaf
Alfredo Perez <alfredopzr1@gmail.com>
2019-07-24 18:40:43 +0200

    Fixing modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to root_path(signed_in: 'false'), notice: "You need to sign in to view"
-    end
-  end
+  # def authenticate_user!
+  #   if user_signed_in?
+  #     super
+  #   else
+  #     redirect_to root_path(signed_in: 'false'), notice: "You need to sign in to view"
+  #   end
+  # end
 end
