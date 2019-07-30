de66cc45b3eddd6f7ace2ab86709900ff585aaf0
Paulina Lopez <paulielopez93@gmail.com>
2019-07-30 08:24:16 -0500

    FIX WIO adding new api key mapbox and removing unused markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,11 +2,11 @@ import mapboxgl from 'mapbox-gl';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  };
+  // const fitMapToMarkers = (map, markers) => {
+  //   const bounds = new mapboxgl.LngLatBounds();
+  //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  //   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  // };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
