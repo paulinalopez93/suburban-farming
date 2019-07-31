e1714c9ab0196008ac7a9ddb0da514fbe9fcffd5
Paulina Lopez <paulielopez93@gmail.com>
2019-07-31 10:37:19 -0500

    fixed the meta tags so the description appears

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_07_31_144014) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,9 +91,8 @@ ActiveRecord::Schema.define(version: 2019_07_31_144014) do
     t.float "latitude"
     t.float "longitude"
     t.string "name"
-    t.string "photo"
-    t.text "about"
     t.bigint "hub_id"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["hub_id"], name: "index_users_on_hub_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
