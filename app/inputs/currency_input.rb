cc659b7708b274234d7cca0c6ddbcc15ec182e4d
Alfredo Perez <alfredopzr1@gmail.com>
2019-07-31 17:35:54 +0200

    Produce post changes

--- app/inputs/currency_input.rb
+++ app/inputs/currency_input.rb
@@ -2,6 +2,6 @@ class CurrencyInput < SimpleForm::Inputs::Base
   def input(wrapper_options)
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
 
-    @builder.text_field(attribute_name, merged_input_options).html_safe
+    @builder.number_field(attribute_name, merged_input_options).html_safe
   end
 end
