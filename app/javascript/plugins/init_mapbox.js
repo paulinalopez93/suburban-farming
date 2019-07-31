5a43602e7cdd749a1979ae32f1a9ee97c3ab5036
(Simon Martinez) <smartineze1993@gmail.com>
2019-07-31 10:53:06 +0200

    maps are working with marker

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,11 @@
 import mapboxgl from 'mapbox-gl';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElements = document.querySelectorAll('.map');
 
@@ -7,6 +13,7 @@ const initMapbox = () => {
     mapElements.forEach((mapElement) => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     let map = null;
+    mapElement.classList.add("invisible");
 
 
     $("#info-modal-" + mapElement.dataset.id).on('show.bs.modal', function () {
@@ -17,14 +24,21 @@ const initMapbox = () => {
       console.log("MAP LOADING");
     });
     $("#info-modal-" + mapElement.dataset.id).on('shown.bs.modal', function () {
-      window.setTimeout(function() {
-        map.resize();
-        console.log("MAP RESIZED");
-      }, 500);
+      mapElement.classList.remove("invisible");
+      map.resize();
+      console.log("MAP RESIZED");
+      const marker = JSON.parse(mapElement.dataset.marker);
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+      console.log("MARKERS LOADED");
+      fitMapToMarkers(map, [marker]);
+      console.log("MAP FIT TO MARKER");
     });
     $("#info-modal-" + mapElement.dataset.id).on('hide.bs.modal', function () {
       if (map) {
         map.remove();
+        mapElement.classList.add("invisible");
       }
       console.log("MAP REMOVED");
       map = null;
