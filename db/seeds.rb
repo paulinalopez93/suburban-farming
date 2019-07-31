18d3e6311064cf8b9bb73ed4bbf5e731605d206c
(Simon Martinez) <smartineze1993@gmail.com>
2019-07-31 16:59:52 +0200

    add pgoto column to user and add photo to user on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.destroy_all
 
 puts 'Creating products & user...'
 
-simon = User.create!(name: "Simon", email:"simon@gmail.com", password:"hola123", address: "Calle Juan de Vera 15 Madrid")
+simon = User.create!(name: "Simon", email:"simon@gmail.com", password:"hola123", address: "Calle Juan de Vera 15 Madrid", photo: "userSimon.jpg")
 
 alex = User.create!(name: "Alex", email:"alex@gmail.com", password:"hola123", address: "Calle Castello 8 Madrid")
 
@@ -16,7 +16,7 @@ adel= User.create!(name: "Adel", email:"adel@gmail.com", password:"hola123", add
 
 paulina = User.create!(name: "Paulina", email:"paulina@gmail.com", password:"hola123", address: "Calle de Manuela Malasana 10 Madrid")
 
-alfredo = User.create!(name: "Alfredo", email:"alfredo@gmail.com", password:"hola123", address: "Calle de Benigno Soto 9 Madrid")
+alfredo = User.create!(name: "Alfredo", email:"alfredo@gmail.com", password:"hola123", address: "Calle de Benigno Soto 9 Madrid", photo: "userSimon.jpg")
 
 sofia = User.create!(name: "Sofia", email:"sofia@gmail.com", password:"hola123", address: "Calle de Ponzano 11 Madrid")
 
