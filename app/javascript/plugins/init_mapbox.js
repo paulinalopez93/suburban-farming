0c0551083bc85bb88c372c4dbfecbee4b70a62cc
(Simon Martinez) <smartineze1993@gmail.com>
2019-07-31 10:41:53 +0200

    THEY WORK

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,33 +2,34 @@ import mapboxgl from 'mapbox-gl';
 
 const initMapbox = () => {
   const mapElements = document.querySelectorAll('.map');
-  // const fitMapToMarkers = (map, markers) => {
-  //   const bounds = new mapboxgl.LngLatBounds();
-  //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  //   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  // };
 
-// only build a map if there's a div#map to inject into
   if (mapElements) {
     mapElements.forEach((mapElement) => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     let map = null;
 
 
+    $("#info-modal-" + mapElement.dataset.id).on('show.bs.modal', function () {
+      map = new mapboxgl.Map({
+        container: 'map-' + mapElement.dataset.id,
+        style: 'mapbox://styles/mapbox/light-v9',
+      });
+      console.log("MAP LOADING");
+    });
     $("#info-modal-" + mapElement.dataset.id).on('shown.bs.modal', function () {
-        map = new mapboxgl.Map({
-          container: 'map-' + mapElement.dataset.id
-        });
+      window.setTimeout(function() {
         map.resize();
-      })
-    $("#info-modal-" + mapElement.dataset.id).on('hidden.bs.modal', function () {
+        console.log("MAP RESIZED");
+      }, 500);
+    });
+    $("#info-modal-" + mapElement.dataset.id).on('hide.bs.modal', function () {
       if (map) {
         map.remove();
       }
+      console.log("MAP REMOVED");
       map = null;
     });
   });
-
   }
 }
 
